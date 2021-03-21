6747fd6d34cf90a3ade0ee9d3dfead58750675bb
guanglancomet <373033833@qq.com>
2021-03-21 21:34:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+cd package/lean/
+git clone https://github.com/fw876/helloworld.git
